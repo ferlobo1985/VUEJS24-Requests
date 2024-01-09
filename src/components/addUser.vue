5d27683ad8d98c7ef92d3fef86463ddd6c54092e
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-01-09 03:01:42 -0300

    04-Posting
    04-Posting

--- src/components/addUser.vue
+++ src/components/addUser.vue
@@ -1,35 +1,51 @@
 <template>
-    <h2>Add users</h2>
-    <div>
-        <div class="form-group mb-3">
-            <label>Name</label>
-            <input class="form-control" type="text" v-model="user.name">
-        </div>
-
-        <div class="form-group mb-3">
-            <label>Lastname</label>
-            <input class="form-control" type="text" v-model="user.lastname">
-        </div>
-
-        <button 
-            class="btn btn-primary" 
-            @click.prevent="submitForm"
-        >Submit</button>
+  <h2>Add users</h2>
+  <div v-show="loading">
+    <app-loader />
+  </div>
+  <div v-show="!loading">
+    <div class="form-group mb-3">
+      <label>Name</label>
+      <input class="form-control" type="text" v-model="user.name" />
     </div>
+
+    <div class="form-group mb-3">
+      <label>Lastname</label>
+      <input class="form-control" type="text" v-model="user.lastname" />
+    </div>
+
+    <button class="btn btn-primary" @click.prevent="submitForm">Submit</button>
+  </div>
 </template>
 
 <script setup>
-    import { reactive } from 'vue';
+import axios from "axios";
+import { useToast } from "vue-toast-notification";
+import { reactive, ref } from "vue";
 
-    const user = reactive({
-        name:'',
-        lastname:''
-    });
+const $toast = useToast();
+const loading = ref(false);
+const user = reactive({
+  name: "",
+  lastname: "",
+});
 
-    const submitForm = () => {
-        console.log(user);
-    }
+const submitForm = () => {
+  loading.value = true;
 
-
-
+  axios({
+    method: "POST",
+    url: "http://localhost:3004/users",
+    data: user,
+  })
+    .then(() => {
+      $toast.success("Great dude !!!");
+    })
+    .catch((error) => {
+      $toast.error("Sorry, something went wrong");
+    })
+    .finally(()=>{
+        loading.value = false;
+    })
+};
 </script>
