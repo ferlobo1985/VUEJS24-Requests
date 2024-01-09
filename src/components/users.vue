01d3433bd13301c65a270362a53be11aef1e14dc
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-01-09 02:36:27 -0300

    02-GET users
    02-GET users

--- src/components/users.vue
+++ src/components/users.vue
@@ -1,18 +1,22 @@
 <template>
     <div class="row">
-        <div class="col-auto mb-4">
+        <div 
+            class="col-auto mb-4"
+            v-for="(user) in data.users"
+            :key="user.id"
+        >
             <div class="card" style="width: 14rem;">
                 <img 
                     class="card-img-top" 
-                    src="https://placebeard.it/300/300"
+                    :src="'https://placebeard.it/300/300?' + user.id  "
                     alt="Card image cap"
                 >
                 <div class="card-body">
                     <div class="card-text">
-                        <strong>Name:</strong> Francis
+                        <strong>Name:</strong> {{ user.name }}
                     </div>
                     <div class="card-text">
-                        <strong>Lastname:</strong>  Jones
+                        <strong>Lastname:</strong> {{ user.lastname }}
                     </div>
                 </div>
             </div>
@@ -21,5 +25,26 @@
 </template>
 
 <script setup>
+    import axios from 'axios';
+    import { reactive, onMounted } from 'vue';
+
+    const data = reactive({
+        user:[]
+    })
+
+    const loadUsers = () => {
+        axios.get(`http://localhost:3004/users`)
+        .then(response=>{
+            data.users =  response.data;
+        })
+        .catch(errro=>{
+            console.log(error)
+        })
+    }
+
+    onMounted(()=>{
+        loadUsers();
+    })
+  
 
 </script>
